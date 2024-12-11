e802cbc7fa497b25d7b77492924ada190e9e24eb
EdgarK7 <edgarukas505@gmail.com>
2024-12-11 12:04:58 -0800

    modifie (8) added sfx game done

--- game.h
+++ game.h
@@ -8,25 +8,29 @@ class Game
 {
 public:
 	Game();
-	Block GetRandomBlock();
-	vector<Block> GetAllBlocks();
+	~Game();
 	void Draw();
 	void HandleInput();
 	void MoveBlockLeft();
 	void MoveBlockRight();
 	void MoveBlockDown();
-	Grid grid;
 	bool gameOver;
 	int score;
+	Music music;
 
 private:
+	Grid grid;
 	bool IsBlockOutside();
 	void RotateBlock();
 	void LockBlock();
 	bool BlockFits();
 	void Reset();
 	void UpdateScore(int linesClreared, int moveDownPoints);
+	Block GetRandomBlock();
+	vector<Block> GetAllBlocks();
 	vector<Block> blocks;
 	Block currentBlock;
 	Block nextBlock;
+	Sound rotateSound;
+	Sound clearSound;
 };
